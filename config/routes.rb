9e49c36f0ac49924e0daa591ea0c45fedd557a0e
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2017-01-14 22:59:40 -0500

    vainas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "categories#index"
+
   resources :advertisements, only: [:index, :show, :create, :new]
   resources :categories, only: [:index, :show, :create, :new, :update, :destroy, :edit]
 end
