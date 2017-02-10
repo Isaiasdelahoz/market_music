9dd7754beef1d5602c46a283120ff63a79fe7a88
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2017-02-10 15:29:42 -0500

    test fixed

--- spec/integration/users/searching_advertisement_spec.rb
+++ spec/integration/users/searching_advertisement_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper.rb'
  feature 'search advertisement' do 
     scenario 'can find a advertisement' do 
       
-      create(:advertisement)
+      create(:advertisement, name: 'nuevo_producto')
 
       create(:advertisement, name: 'otro')
       
@@ -12,7 +12,7 @@ require 'rails_helper.rb'
 
       click_button 'Buscar' 
       
-      expect(page).to have_content('nuevo')
+      expect(page).to have_content('nuevo_producto')
 
       expect(page).not_to have_content('otro')
 
